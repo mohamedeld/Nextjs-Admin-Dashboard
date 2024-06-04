bffe79d6f053b5e792380f0781d39c4a90bfde0d
mohamedeld <mohamed.azoz20010@gmail.com>
2024-06-04 21:23:26 +0300

    add sub header

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,16 @@
+import DashboardCard from "@/components/dashboard/dashboardCard";
+import { Folders, MessageCircle, Newspaper, Users } from "lucide-react";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
+    <div className="flex flex-col md:flex-row justify-between gap-5 mb-5">
+      <DashboardCard title="Posts" count={100} icon={<Newspaper className='text-slate-500' size={72}/>}/>
+      <DashboardCard title="Categories" count={12} icon={<Folders className='text-slate-500' size={72}/>}/>
+      <DashboardCard title="Users" count={750} icon={<Users className='text-slate-500' size={72}/>}/>
+      <DashboardCard title="Comments" count={1200} icon={<MessageCircle className='text-slate-500' size={72}/>}/>
+    </div>
     </>
   );
 }
